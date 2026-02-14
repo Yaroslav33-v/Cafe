39afe6b05e65105e3ce3f519e974dd6c75dda3d4
Yaroslav33-v <yaroslav.vasilev.0604@mail.ru>
2026-02-15 01:08:48 +0300

    Начал делать тг бота

---
+++ TelegramBot/TelegramBot/Program.cs
@@ -0,0 +1,26 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+namespace TelegramBot
+{
+    internal class Program
+    {
+        private static ITelegramBotClient _botClient;
+        private static ReceiverOptions _receiverOptions;
+        static void Main()
+        {
+            using var cts = new CancellationTokenSource();
+            _receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates =
+                [
+                    UpdateType.Message,
+                ],
+                DropPendingUpdates = true,
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
