7807e57fa3b29a3e1652a2f7c4266d9438afc84c
228demonik228@list.ru <228demonik228@list.ru>
2026-03-12 19:17:51 +0300

    Добавил try-catch и логирование исключениц

--- CafeWeb/Services/CafeService.cs
+++ CafeWeb/Services/CafeService.cs
@@ -1,5 +1,6 @@
 ﻿using CafeWeb.Models;
 using Dapper;
+using Npgsql;
 using System.Data;
 
 namespace CafeWeb.Services
@@ -7,22 +8,28 @@ namespace CafeWeb.Services
     public class CafeService : ICafeService
     {
         private readonly IDbConnection _connection;
+        private readonly ILogger<CafeService> _logger;
 
-        public CafeService(IDbConnection connection)
+
+        public CafeService(IDbConnection connection, ILogger<CafeService> logger)
         {
             _connection = connection ?? throw new ArgumentNullException(nameof(connection));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
         }
 
         public async Task<List<Category>> GetFoods()
         {
-            // Получаем все категории
-            var categories = (await _connection
-                .QueryAsync<Category>("SELECT category_id AS Id, name AS Name FROM public.categories"))
-                .ToList();
+            try
+            {
+                // Получаем все категории
+                var categories = (await _connection
+                    .QueryAsync<Category>("SELECT category_id AS Id, name AS Name FROM public.categories"))
+                    .ToList();
 
-            // Получаем все блюда
-            var foods = (await _connection
-                .QueryAsync<Food>(@"
+                // Получаем все блюда
+                var foods = (await _connection
+                    .QueryAsync<Food>(@"
             SELECT 
                 food_id AS Id, 
                 food_name AS Name, 
@@ -33,33 +40,21 @@ namespace CafeWeb.Services
                 image_address AS ImageAddress,
                 category_id AS CategoryId
             FROM public.food"))
-                .ToList();
+                    .ToList();
 
-            // Группируем блюда по категориям
-            foreach (var category in categories)
-            {
-                category.Foods = foods.Where(f => f.CategoryId == category.Id).ToList();
-            }
-
-            return categories;
-        }
-        public List<Category> GetFoods1()
-        {
-
-            // как реализовать sql запрос возвращающий json
-            // получаем все из category
-            // передаем в return
-
-            List<Category> categories = [ 
-                new Category{
-                    Name = "яйца",
-                    Foods = [
-                        new Food{Name = "4 Творожка", Price = 1488},
-                        new Food{Name = "Фа пепе шнейне", Price = 1337}
-                    ]
+                // Группируем блюда по категориям
+                foreach (var category in categories)
+                {
+                    category.Foods = foods.Where(f => f.CategoryId == category.Id).ToList();
                 }
-            ];
-            return categories;
+
+                return categories;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Непридвиденная внутренняя ошибка: " + ex.Message);
+                throw new Exception("Непредвиденная внутренняя ошибка");
+            }
         }
     }
 }
