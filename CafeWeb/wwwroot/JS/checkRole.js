2f1cbfd657a1c22ba2309682c26d8fbca8413ad7
Георгий Бурьянов <buryanov.2005@mail.ru>
2026-03-16 13:27:20 +0300

    сделал переход в админские угодия

---
+++ CafeWeb/wwwroot/JS/checkRole.js
@@ -0,0 +1,14 @@
+﻿const adminTeleportation = document.getElementById('admin-relocation');
+adminTeleportation.addEventListener('click', async () => {
+    try {
+        const response = await fetch(`/me`);
+        const data = await response.json();
+
+        if (data.role === "admin") {
+            document.location.replace("/admin/index");
+        }
+    }
+    catch {
+
+    }
+});
