0f25fde52b55710c1b1568339529643a6ef76925
Yaroslav33-v <yaroslav.vasilev.0604@mail.ru>
2026-04-23 19:48:59 +0300

    Сделал обновление кнопки для избранного, написал дальнейшие задачи

--- CafeWeb/wwwroot/JS/main.js
+++ CafeWeb/wwwroot/JS/main.js
@@ -27,14 +27,23 @@
     }
 }
 
-async function updateFavourite(foodId) {
+async function updateFavourite(foodId, buttonElement) {
     try {
         const response = await fetch(`/cafe/updatefavourite?foodId=${foodId}`);
         const data = await response.json();
 
+        let btnText = buttonElement.textContent;
+
         if (response.ok && data.success) {
             // Успешное добавление
             showNotification(data.message, 'success');
+            if (btnText === 'Добавить в избранное') {
+                buttonElement.textContent = 'Удалить из избранного';
+            } else {
+                buttonElement.textContent = 'Добавить в избранное';
+            }
+            // Здесь надо как-то изменять блок избранных
+            // Самый простой вариант: Подождать 2-3 секунды и обновить страницу, но можешь сделать че то другое
         } else if (response.status === 404) {
             // Блюдо не найдено
             showNotification('Блюдо не найдено', 'error');
@@ -52,3 +61,29 @@ async function updateFavourite(foodId) {
         );
     }   
 }
+
+// Над этим тоже рекомендую подумать, потому что без перезагрузки страницы, у него начинаются проблемы
+document.addEventListener('DOMContentLoaded', () => {
+    const favoriteDiv = document.querySelector('.menu-category[data-category-name="Избранное"]');
+    if (favoriteDiv) {
+        const ids = Array.from(favoriteDiv.querySelectorAll('.card')).map(card => parseInt(card.dataset.cardId));
+        const cards = document.querySelectorAll('.card');
+
+        cards.forEach(card => {
+            const foodId = parseInt(card.dataset.cardId);
+            const button = card.querySelector('button[onclick*="updateFavourite"]');
+
+            if (button && ids.includes(foodId)) {
+                button.textContent = 'В избранном';
+
+                button.onmouseenter = () => {
+                    button.textContent = 'Удалить из избранного';
+                };
+
+                button.onmouseleave = () => {
+                    button.textContent = 'В избранном';
+                };
+            }
+        });
+    }
+})
