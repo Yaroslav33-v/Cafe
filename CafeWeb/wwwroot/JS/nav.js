0d3a9deea06f7ddba365904faf7add831ff09dfa
Георгий Бурьянов <buryanov.2005@mail.ru>
2026-05-24 23:24:41 +0300

    добавил навигацию на мейн но не доделал

---
+++ CafeWeb/wwwroot/JS/nav.js
@@ -0,0 +1,24 @@
+﻿const links = document.querySelectorAll('aside nav ul a');
+const sections = document.querySelectorAll('.menu-category');
+
+function updateActive() {
+    const scrollPos = window.scrollY + 120;
+
+    sections.forEach(section => {
+        const top = section.offsetTop;
+        const bottom = top + section.offsetHeight;
+
+        if (scrollPos >= top && scrollPos < bottom) {
+            const id = section.getAttribute('id');
+            links.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href') === `#${id}`) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+}
+
+window.addEventListener('scroll', updateActive);
+updateActive();
