78cc662aaf9dbeb4da6f386ad52a01382dc0d1a5
Георгий Бурьянов <buryanov.2005@mail.ru>
2026-06-02 22:22:50 +0300

    about

---
+++ CafeWeb/wwwroot/JS/scroll.js
@@ -0,0 +1,18 @@
+﻿const scrollBtn = document.getElementById('scrollToTop');
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 400) {
+        scrollBtn.classList.add('visible');
+    } else {
+        scrollBtn.classList.remove('visible');
+    }
+});
+
+
+scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
